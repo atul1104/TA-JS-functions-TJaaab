d95ef44383cc731b446a975ce0eebd84b6eb10cf
atul1104 <waghamareatul2@gmail.com>
2021-11-17 10:19:53 +0530

    assignment

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,8 +11,8 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
-  // Your code
+function minToSec(min, conversion = 60) {
+  return min * conversion;
 }
 // - Execute the function with required parameter
 
@@ -26,8 +26,12 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
-  // Your code
+function isInRange(lower, upper, number) {
+  if (number >= lower && number <= upper){
+    return true;
+  }else{
+    return false;
+  }
 }
 // - Execute the function with required parameter
 
@@ -49,8 +53,19 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
-  // Your code
+function calculateBMI(weight, height ) {
+  let bmi = weight / (height * height); 
+    if (bmi < 18.5){
+      return `Underweight`;
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+       return `Normal Weight`;
+    } else if (bmi >= 25 && bmi <= 29.9){
+       return `Overweight`;
+    } else if (bmi >= 30){
+      return `Obese`; 
+    } else{
+      return `Enter valid input`;
+    }
 }
 
 /* 3. appropiateDrinks
@@ -64,8 +79,16 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
-  // Your code
+function appropiateDrinks(age) {
+  if (age < 14){
+    return `drink fruit juice`;
+  } else if (age < 18 && age > 14){
+    return `drink soda`;
+  } else if (age < 21 && age > 18){
+    return `drink fruit-glavored beer`;
+  } else {
+    return `drink throat-piercing vodka`;
+  }
 }
 
 /* 4. Add two numers or string
@@ -79,8 +102,14 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
-  // Your code
+function sum(number, string) {
+  if(number + number){
+    return +(number + number);
+  }else if(string + string){
+    return String(`string string`);
+  }else{
+    return `Enter valid input`;
+  }
 }
 
 // Function Test
